df232a89569c020019ab5599cbb4d2be94f6fa35
Vladyslav Nehodiuk <pearmadn@email.com>
2024-03-28 19:45:41 +0200

    Add signIn html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import * as $ from 'jquery';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, MatFormFieldModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+console.log(`jQuery version: ${$.fn.jquery}`);
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  @ViewChild('createAccountButton1')
+  createAccountButton1: ElementRef<HTMLInputElement>;
+  @ViewChild('createAccountButton2')
+  createAccountButton2: ElementRef<HTMLInputElement>;
 
+  ngAfterViewInit() {
+    this.createAccountButton1.nativeElement.addEventListener(
+      'click',
+      this.toogleLoginAnimation
+    );
+     this.createAccountButton2.nativeElement.addEventListener(
+       'click',
+       this.toogleLoginAnimation
+     );
+  }
+
+  toogleLoginAnimation(): void {
+    $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+  }
 }
